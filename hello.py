fc20c74980e8ceec5225b5917a46fae208905fa2
ICT-Lab <murengezisteven5@gmail.com>
2020-02-28 10:25:34 +0200

    my first commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+# from mymodulee import person1
+# # a=mymodulee.person1["age"]
+# # print(a)
+# print(person1["age"])
+#important thing to return date year
+# import datetime
+# x=datetime.datetime.now()
+# print(x.strftime("%B"))
+username=input("enter username:")
+print("username is:"+username)
